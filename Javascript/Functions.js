b891ce0956ea5c4b208e0af5ff260f1c28382eb9
Yosemr12 <moncadarodriguezyoselin@gmail.com>
2024-02-12 11:25:46 -0600

    Agregue ccs para mi formukario

--- Javascript/Functions.js
+++ Javascript/Functions.js
@@ -2,6 +2,7 @@ function changeColor(){
 document.getElementById("palabrita").style.backgroundColor="green";
 document.getElementById("palabrita").textContent="Hola Yose";
 }
+
 function enviarMsg(){
   alert("Hola amigo");
 }
